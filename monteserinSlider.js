d8e090b34bad6ddda88b954c19b5f015524e8333
pablomonteserin@pablomonteserin.com <pp@pp.com>
2014-07-30 11:53:16 +0200

    cambios

--- monteserinSlider.js
+++ monteserinSlider.js
@@ -1,21 +1,45 @@
-var i =0;
+
+jQuery.fn.monteserinSlider = function(event){
 var fadeInTime=2000;
 var fadeOutTime=2000;
 var $lista;
-var listaLength=0;
-jQuery.fn.monteserinSlider = function(event){
+var listaLength=5;
+var maxWidth=0;
+var maxHeight=0;
+
 	$lista = $(this).find("li");
+
+	$lista.each(function(){
+		if($(this).width()>maxWidth)maxWidth=$(this).width();
+		if($(this).height()>maxHeight)maxHeight=$(this).height();
+	});
+
+	$(this).css({"height":maxHeight+"px", "width":maxWidth});
 	listaLength = $lista.length;
-	if(event.fadeInTime !== undefined){fadeInTime=event.fadeInTime}
-	if(event.fadeOutTime !== undefined){fadeOutTime=event.fadeOutTime}
-	hacerSlider(i);      
+	if(event !== undefined){
+		if(event.fadeInTime !== undefined)fadeInTime=event.fadeInTime;
+		if(event.fadeOutTime !== undefined)fadeOutTime=event.fadeOutTime;
+	}else{
+		var event={};
+	}
+	event.i = 0;
 
-}
+	hacerSlider(event.i);      
+
+
+
+
 function hacerSlider(i){
-		 $lista.eq(i).fadeIn(fadeInTime, function(){
-		 	$lista.fadeOut(fadeOutTime);
-		 if(i>=listaLength)window.i=-1;
-		 	hacerSlider(++window.i);
+	event.i=event.i+1;
+	$lista.eq(i).fadeIn(fadeInTime, function(){
+		$lista.fadeOut(fadeOutTime);
+		if(event.i>=listaLength)event.i=0;
+		hacerSlider(event.i);
 
-		 });
+	});
+}
+
+
+
+
 }
